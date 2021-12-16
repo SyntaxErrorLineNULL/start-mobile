9aa00c9951f9f5953fc6460a5eaed370654ec1a6
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-12-16 22:21:57 +0300

    create components

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     /**
-     * @Route("/hello", name="hello")
+     * @Route("/hello", name="hello", methods={"POST"})
      */
-    public function index(): string
+    public function index(Request $request): string
     {
         return 'hello';
     }
---
+++ src/HTTP/Request/RequestSchema.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\HTTP\Request;
+
+use App\HTTP\RequestValidator\RequestValidator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class RequestSchema
+{
+    const TYPE = 'json';
+
+    private Serializer $serializer;
+    private RequestValidator $requestValidator;
+
+    /**
+     * @param RequestValidator $requestValidator
+     */
+    public function __construct(RequestValidator $requestValidator)
+    {
+        $this->serializer = new Serializer(
+            [new ObjectNormalizer(null, null, null, new ReflectionExtractor()), new ArrayDenormalizer()],
+            ['json' => new JsonEncoder()]
+        );
+        $this->requestValidator = $requestValidator;
+    }
+
+    /*public function getRequestProperty(string $schema, Request $request): object
+    {
+        $schema = $this->serializer->deserialize($request->getBody(), $schema, self::TYPE);
+        $this->requestValidator->validate($schema);
+        return $schema;
+    }*/
+}
