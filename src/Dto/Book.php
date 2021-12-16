99dd82e9a5fc0a8bd19a29b61f1251840c41be52
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-12-16 21:07:51 +0300

    create dto

---
+++ src/Dto/Book.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Dto;
+
+class Book
+{
+    public int $id;
+
+    public string $title;
+
+    public string $description;
+
+    public Author $author;
+
+    /**
+     * @param int $id
+     * @param string $title
+     * @param string $description
+     * @param Author $author
+     */
+    public function __construct(int $id, string $title, string $description, Author $author)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->author = $author;
+    }
+
+}
