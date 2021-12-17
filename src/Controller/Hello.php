557f0c53ef09100360dee8e958c46b4ff968457a
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-12-17 18:17:05 +0300

    this test controller

---
+++ src/Controller/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class Hello
+{
+    public string $name;
+}
--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\HTTP\Request\RequestSchema;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -12,8 +12,9 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello", name="hello", methods={"POST"})
      */
-    public function index(Request $request): string
+    public function index(Request $request, RequestSchema $requestSchema)
     {
-        return 'hello';
+        $request = $requestSchema->getRequestProperty(Hello::class, $request);
+        return $this->json($request->name);
     }
 }
