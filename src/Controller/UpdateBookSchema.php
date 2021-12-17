ec404d34b4c6b36ff7c5e018059155026cd093c8
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-12-17 18:14:37 +0300

    #4 create restful BookController

---
+++ src/Controller/UpdateBookSchema.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class UpdateBookSchema
+{
+    public ?string $title;
+
+    public ?string $description;
+}
